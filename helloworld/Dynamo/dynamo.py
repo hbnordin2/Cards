392ff337eb2503021f0cc06d01a3ebb2e1d85341
Haziq Nordin <hbnordin2@gmail.com>
2018-09-09 16:19:02 +1000

    Added region to dynamo access

--- helloworld/Dynamo/dynamo.py
+++ helloworld/Dynamo/dynamo.py
@@ -4,7 +4,7 @@ class DynamoTable:
     def __init__(self, tableName):
         self.tableName = tableName
 
-        dynamodb = boto3.resource('dynamodb')
+        dynamodb = boto3.resource('dynamodb',region_name='ap-southeast-2')
         self.table = dynamodb.Table(tableName)
 
     def getEverything(self):
